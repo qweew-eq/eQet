556d70c040b9026f31b3fd1c55ae98df1ab57c59
QwEEW <texture.id.not.found@gmail.com>
2026-01-22 18:02:16 +0500

    Add initMap function to initialize a map

---
+++ src/map/map.js
@@ -0,0 +1,11 @@
+import maplibregl from 'maplibre-gl';
+
+export function initMap() {
+  const map = new maplibregl.Map({
+    container: 'map',
+    style: 'https://api.maptiler.com/maps/019bdb6b-5300-7f9e-9627-cfc67708b678/style.json?key=r4Ffbnt8100yaV2jJk5e',
+    center: [135.0, 35.0],
+    zoom: 4
+  });
+  return map;
+}
