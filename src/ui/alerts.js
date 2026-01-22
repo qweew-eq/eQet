c3f464dcddd2415e94e5d391391701c1ac559859
QwEEW <texture.id.not.found@gmail.com>
2026-01-22 18:14:12 +0500

    Refactor alert sounds into AudioPlayer module

--- src/ui/alerts.js
+++ src/ui/alerts.js
@@ -1,13 +1,14 @@
-const sounds = {};
-
-['eq','3upper','warn','cancel','lastupd','upd','tsunamiforecast','tsunamiadvisory','tsunamiwarn','tsunamimajor','drill',
- '1','2','3','4','5-','5+','6-','6+','7','cancelspeak','PLUM'].forEach(name=>{
-  sounds[name] = new Audio(`/sounds/${name}.wav`);
-});
-
-export function playAlert(eew) {
-  if(eew.isAssumption) sounds['PLUM'].play();
-  else if(!eew.isFinal) sounds['upd'].play();
-  else if(eew.maxIntensity >= 5) sounds['warn'].play();
-  else sounds['eq'].play();
-}
+// src/ui/alerts.js
+export const AudioPlayer = {
+  _cache: {},
+  play(fileName) {
+    if (!fileName) return;
+    let audio = this._cache[fileName];
+    if (!audio) {
+      audio = new Audio(`/sounds/${fileName}`);
+      this._cache[fileName] = audio;
+    }
+    audio.currentTime = 0;
+    audio.play().catch(e => console.warn('Audio play failed', e));
+  }
+};
