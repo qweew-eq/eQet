ee34538a7ca626b18edfe58f3e8269a7733ed832
QwEEW <texture.id.not.found@gmail.com>
2026-01-23 13:28:47 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // THIS IS THE FIX: It makes paths relative so they don't 404
+  base: './',
   root: '.',
   build: {
     outDir: 'dist'
