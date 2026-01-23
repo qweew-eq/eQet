24564b32e898bba2c7633e2f3021e213126007ac
QwEEW <texture.id.not.found@gmail.com>
2026-01-23 21:52:44 +0500

    Update wolfx.js

--- src/ws/wolfx.js
+++ src/ws/wolfx.js
@@ -1,37 +1,28 @@
-import { connectWolfx } from './wolfx.js';
-import { updatePanel } from '../ui/panel.js';
-import { updateEpicenter } from '../map/marker.js';
-import { autoPan } from '../map/autopan.js';
-import { AudioPlayer } from '../ui/alerts.js';
+// src/ws/wolfx.js
 
-export const eewManager = {
-  activeEEWs: [],
+export function connectWolfx(onMessage) {
+    const ws = new WebSocket('wss://ws-api.wolfx.jp/jma_eew');
 
-  async start(map) {
-    await this.fetchHistory(map);
+    ws.onopen = () => {
+        console.log('Connected to Wolfx EEW');
+    };
 
-    connectWolfx((eew) => {
-      if (eew.isCancel) {
-        this.activeEEWs = [];
-      } else {
-        this.activeEEWs = [eew];
-      }
+    ws.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        
+        // Filter for JMA EEW messages
+        if (data.type === 'jma_eew' || data.Type === 'jma_eew') {
+            onMessage(data);
+        }
+    };
 
-      updatePanel(eew);
-      updateEpicenter(eew, map);
-      AudioPlayer(eew);
-      
-      // Don't autoPan if it's a cancellation (marker handles zoom out)
-      if (!eew.isCancel) autoPan(map, eew);
-    });
-  },
+    ws.onerror = (err) => {
+        console.error('WebSocket Error:', err);
+    };
 
-  async fetchHistory(map) {
-    try {
-      const res = await fetch('https://api.wolfx.jp/jma_eew.json');
-      const data = await res.json();
-      if (data) updatePanel(data);
-      autoPan(map, null); // Start overview
-    } catch (e) { autoPan(map, null); }
-  }
-};
+    ws.onclose = () => {
+        console.log('Disconnected. Reconnecting...');
+        setTimeout(() => connectWolfx(onMessage), 5000);
+    };
+}
+
