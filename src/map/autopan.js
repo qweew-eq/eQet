7f1aa9df3a478e030cfa46c097dfaa824c665874
QwEEW <texture.id.not.found@gmail.com>
2026-01-23 20:54:33 +0500

    Update autopan.js

--- src/map/autopan.js
+++ src/map/autopan.js
@@ -1,3 +1,16 @@
-export function autoPan(map, coords) {
-  map.easeTo({ center: [coords.lon, coords.lat], zoom: 6, duration: 500 });
+export function autoPan(map, eew) {
+  if (!map) return;
+
+  // Zoom out to Japan overview
+  if (!eew) {
+    map.flyTo({ center: [138.0, 38.0], zoom: 4.5, speed: 0.6, essential: true });
+    return;
+  }
+
+  // Zoom in or ease to quake
+  if (eew.Serial > 1) {
+    map.easeTo({ center: [eew.Longitude, eew.Latitude], duration: 2000 });
+  } else {
+    map.flyTo({ center: [eew.Longitude, eew.Latitude], zoom: 6.5 });
+  }
 }
