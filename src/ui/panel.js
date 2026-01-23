d201dd6de9275b34cdbf3f924df8bd572cfac5ba
QwEEW <texture.id.not.found@gmail.com>
2026-01-23 21:41:34 +0500

    Update panel.js

--- src/ui/panel.js
+++ src/ui/panel.js
@@ -17,11 +17,8 @@ export function updatePanel(eew) {
 
   const maxInt = eew.MaxIntensity || 'FUMEI';
   const imgName = MAXINT_IMAGES[maxInt] || MAXINT_IMAGES.FUMEI;
-  
-  // Red for Warn (Alert), Orange for Forecast
   const pillColor = eew.isWarn ? '#ff0000' : '#ff8c00';
   
-  // Format the title text
   let reportTitle = '';
   if (eew.isCancel) {
     reportTitle = '緊急地震速報 (取消)';
@@ -35,10 +32,12 @@ export function updatePanel(eew) {
 
   panel.innerHTML = `
     <div class="icon-gutter">
-      <img src="/icon.png" class="side-icon" alt="Home">
-      <img src="/apple-touch-icon.png" class="side-icon" alt="Waves">
+      <img src="/images/imagesicon/icon.png" class="side-icon" alt="Home">
+      <img src="/images/imagesicon/shake.png" class="side-icon" alt="Shake">
+      <img src="/images/imagesicon/info.png" class="side-icon" alt="Info">
+      <img src="/images/imagesicon/wave.png" class="side-icon" alt="Wave">
       <div style="flex-grow: 1;"></div>
-      <img src="/android-chrome-192x192.png" class="side-icon" id="settings-btn" style="margin-bottom: 20px;" alt="Settings">
+      <img src="/images/imagesicon/settings.png" class="side-icon" id="settings-btn" style="margin-bottom: 20px;" alt="Settings">
     </div>
 
     <div class="panel-content">
@@ -46,25 +45,25 @@ export function updatePanel(eew) {
         ${reportTitle}
       </div>
 
-      <div class="intensity-block">
-        <img src="/shindoimg/panelintensity/${imgName}" class="shindo-img" onerror="this.src='/shindoimg/panelintensity/shindo_unk_int.png'">
-        <div class="meta-info">
-          ${timeParts[0]}<br/>
-          <span style="font-size: 18px; font-weight: bold;">${timeParts[1]} JST</span>
+      <div class="intensity-block" style="display: flex; gap: 15px; align-items: flex-start;">
+        <img src="/shindoimg/panelintensity/${imgName}" class="shindo-img" style="width: 80px; height: 80px;">
+        <div class="meta-info" style="font-size: 13px; line-height: 1.4;">
+          発生日時: ${timeParts[0]}<br/>
+          <span style="font-size: 16px; font-weight: bold;">${timeParts[1]} JST</span><br/>
+          <span style="font-size: 18px; font-weight: bold; display: block; margin-top: 5px;">${eew.Hypocenter || '震源地不明'}</span>
         </div>
       </div>
 
-      <span class="location-text">${eew.Hypocenter || '震源地不明'}</span>
-
-      <div style="text-align: right; font-size: 15px; line-height: 2.0; color: #eee; margin-top: 10px;">
-        マグニチュード : <span style="font-weight: bold;">M${eew.Magunitude}</span><br/>
+      <div style="text-align: right; font-size: 15px; margin-top: 10px; color: #eee;">
+        マグニチュード : <span style="font-weight: bold;">${eew.Magunitude}</span><br/>
         深さ : <span style="font-weight: bold;">${eew.Depth}km</span>
       </div>
       
-      <div id="status-log" style="font-size: 13px; color: #bbb; margin-top: 20px; border-top: 1px solid #444; padding-top: 10px;">
-        ${eew.isAssumption ? '※PLUM法による推定を含む<br/>' : ''}
-        ${eew.isCancel ? '<span style="color: #ff4444; font-weight: bold;">この速報は取り消されました。</span>' : 
-          (eew.isFinal ? '最終報を受信しました。' : '情報を更新中...')}
+      <div class="eq-reports-area" style="margin-top: 20px;">
+        地震情報 :
+        <div id="status-log" style="font-size: 13px; color: #bbb; margin-top: 5px;">
+          ${eew.isCancel ? '<span style="color: #ff4444; font-weight:bold;">この速報は取り消されました。</span>' : ''}
+        </div>
       </div>
     </div>
   `;
