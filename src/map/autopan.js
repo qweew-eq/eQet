d418ad826a01bd1bfcfeed7380d2b47a608076f0
QwEEW <texture.id.not.found@gmail.com>
2026-01-25 02:20:07 +0500

    Update autopan.js
    A MA

--- src/map/autopan.js
+++ src/map/autopan.js
@@ -1,16 +1,18 @@
 export function autoPan(map, eew) {
   if (!map) return;
 
-  // Zoom out to Japan overview
+  // Check the Setting!
+  const isEnabled = localStorage.getItem('autoPan') === 'on';
+  if (!isEnabled) return; 
+
   if (!eew) {
     map.flyTo({ center: [138.0, 38.0], zoom: 4.5, speed: 0.6, essential: true });
     return;
   }
 
-  // Zoom in or ease to quake
   if (eew.Serial > 1) {
     map.easeTo({ center: [eew.Longitude, eew.Latitude], duration: 2000 });
   } else {
-    map.flyTo({ center: [eew.Longitude, eew.Latitude], zoom: 6.5 });
+    map.flyTo({ center: [eew.Longitude, eew.Latitude], zoom: 6.5, essential: true });
   }
 }
