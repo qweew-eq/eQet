5d11bb19ad411d899e6005fa53c88992726f78b9
QwEEW <texture.id.not.found@gmail.com>
2026-01-24 21:39:07 +0500

    Update map.js
    Temporarily updates key as I will be moving it to somewhere else.

--- src/map/map.js
+++ src/map/map.js
@@ -3,7 +3,7 @@ import maplibregl from 'maplibre-gl';
 export function initMap() {
   const map = new maplibregl.Map({
     container: 'map',
-    style: 'https://api.maptiler.com/maps/019bdb6b-5300-7f9e-9627-cfc67708b678/style.json?key=r4Ffbnt8100yaV2jJk5e',
+    style: 'https://api.maptiler.com/maps/019bdb6b-5300-7f9e-9627-cfc67708b678/style.json?key=IAxgKtygqhnYR1RJh6rS',
     center: [135.0, 35.0],
     zoom: 4
   });
