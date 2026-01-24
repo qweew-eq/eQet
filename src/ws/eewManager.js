67a4087187135bc5bf42d29e2a841f8956444cf1
QwEEW <texture.id.not.found@gmail.com>
2026-01-24 13:49:34 +0500

    Update eewManager.js

--- src/ws/eewManager.js
+++ src/ws/eewManager.js
@@ -5,28 +5,19 @@ import { autoPan } from '../map/autopan.js';
 import { tsunamiManager } from './tsunamiManager.js';
 
 export const eewManager = {
-  activeEEWs: [], // Essential for waveAnimation in main.js
+  activeEEWs: [], 
   historyQuakes: [],
 
   async start(map) {
-    // 1. Initial Fetch: Get last 4 confirmed quakes
     await this.fetchEqHistory();
 
-    // 2. Start Live EEW Feed (Wolfx)
     connectWolfx((eew) => {
-      if (eew.isCancel) {
-        this.activeEEWs = [];
-      } else {
-        this.activeEEWs = [eew];
-      }
-
-      // Update UI & Map
+      this.activeEEWs = eew.isCancel ? [] : [eew];
       updatePanel(eew, this.historyQuakes);
       updateEpicenter(eew, map);
       if (!eew.isCancel) autoPan(map, eew);
     });
 
-    // 3. Start Live Confirmed & Tsunami Feed (P2PQuake)
     this.connectP2PWS();
   },
 
@@ -34,32 +25,24 @@ export const eewManager = {
     try {
       const res = await fetch('https://api.p2pquake.net/v2/history?codes=551&limit=4');
       this.historyQuakes = await res.json();
-      // Render panel immediately with history data
       updatePanel(null, this.historyQuakes);
     } catch (e) {
-      console.error("P2P History fetch failed", e);
+      updatePanel(null, []);
     }
   },
 
   connectP2PWS() {
     const ws = new WebSocket('wss://api.p2pquake.net/v2/ws');
-    
     ws.onmessage = (event) => {
       const data = JSON.parse(event.data);
-      
-      // 551: Confirmed Earthquake Info
       if (data.code === 551) {
         this.historyQuakes.unshift(data);
         this.historyQuakes = this.historyQuakes.slice(0, 4);
-        const currentEEW = this.activeEEWs.length > 0 ? this.activeEEWs[0] : null;
-        updatePanel(currentEEW, this.historyQuakes);
-      } 
-      // 552: Tsunami Notice
-      else if (data.code === 552) {
+        updatePanel(this.activeEEWs[0] || null, this.historyQuakes);
+      } else if (data.code === 552) {
         tsunamiManager.handleTsunami(data);
       }
     };
-
     ws.onclose = () => setTimeout(() => this.connectP2PWS(), 5000);
   }
 };
