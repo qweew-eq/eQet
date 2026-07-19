274e1fd37932c67d9875539ccdf4c9d0f4ed5b0a
QwEEW <texture.id.not.found@gmail.com>
2026-07-19 17:33:56 +0500

    Update MapTiler API key placeholder in initMap

--- src/map/map.js
+++ src/map/map.js
@@ -3,7 +3,7 @@ import maplibregl from 'maplibre-gl';
 export function initMap() {
   const map = new maplibregl.Map({
     container: 'map',
-    style: 'https://api.maptiler.com/maps/019bdb6b-5300-7f9e-9627-cfc67708b678/style.json?key=IAxgKtygqhnYR1RJh6rS',
+    style: 'https://api.maptiler.com/maps/019bdb6b-5300-7f9e-9627-cfc67708b678/style.json?key=__MAPTILER_KEY__',
     center: [135.0, 35.0],
     zoom: 4
   });
