4ed34d7cacb13d67b384ed1720322dad5f26ea5a
QwEEW <texture.id.not.found@gmail.com>
2026-07-19 17:52:37 +0500

    Enhance map initialization with transformRequest
    Add transformRequest to set User-Agent for MapTiler API

--- src/map/map.js
+++ src/map/map.js
@@ -5,7 +5,17 @@ export function initMap() {
     container: 'map',
     style: 'https://api.maptiler.com/maps/019bdb6b-5300-7f9e-9627-cfc67708b678/style.json?key=__MAPTILER_KEY__',
     center: [135.0, 35.0],
-    zoom: 4
+    zoom: 4,
+    transformRequest: (url, resourceType) => {
+      if (url.includes('api.maptiler.com')) {
+        return {
+          url: url,
+          headers: {
+            'User-Agent': 'DesktopTaurivo1dd'
+          }
+        };
+      }
+    }
   });
   return map;
 }
